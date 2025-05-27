34344317770dea027a3e91c5ab387423f2c6e92f
a5yuan <134692219+a5yuan@users.noreply.github.com>
2025-05-27 14:29:58 +0800

    element 初始化流程

--- example/hellowrod/App.js
+++ example/hellowrod/App.js
@@ -4,11 +4,11 @@ export const App = {
     //* vue3
     //*  template
     //* rend
-    render(){
-        return h('div','hi,' +  this.msg)
+    render() {
+        return h('div', { id: 'root', class: ['red', 'blue'] }, [h('p', { id: 'red', class: 'red'}, 'hi,red'),h('p', { id: 'blue', class: 'blue'}, 'hi,blue')])
     },
-    setup(){
+    setup() {
 
-        return {msg:'word'}
+        return { msg: 'word' }
     }
 }
--- src/runtime-core/render.js
+++ src/runtime-core/render.js
@@ -1,26 +1,53 @@
 import { createComponentInstance, setupComponent } from "./component"
-export function render(vNode,container){
+import { isObject } from "../share/extend";
+export function render(vNode, container) {
     //* patch
-    patch(vNode,container)
+    patch(vNode, container)
 }
 
-function patch(vNode,container){
+function patch(vNode, container) {
     //* 处理组件
     //* 如何 区分 element 和 component 类型
+    if (typeof vNode.type === 'string') {
+        processElement(vNode, container)
+    } else if (isObject(vNode.type)) {
+        processComponent(vNode, container)
+
+    }
     console.log('vNode', vNode);
-    processComponent(vNode,container)
 }
 
-function processComponent(vNode,container){
-    mountComponent(vNode,container)
+function processComponent(vNode, container) {
+    mountComponent(vNode, container)
 }
-function mountComponent(vNode,container){
+function processElement(vNode, container) {
+    mountElement(vNode, container)
+}
+function mountElement(vNode, container) {
+    //* 挂载元素
+    const { type, props, children } = vNode
+    const el = document.createElement(type)
+    //* child string or Array
+    if(typeof children === 'string'){
+        el.textContent = children
+
+    }else if(Array.isArray(children)){
+        children.forEach(v=>{
+            patch(v,el)
+        })
+    }
+    for (const key in props) {
+        el.setAttribute(key, props[key])
+    }
+    container.append(el)
+}
+function mountComponent(vNode, container) {
     //* 创建 组件实例
     const instance = createComponentInstance(vNode)
     setupComponent(instance)
-    setupRendEffect(instance,container)
+    setupRendEffect(instance, container)
 }
-function setupRendEffect(instance,container){
+function setupRendEffect(instance, container) {
     const subTree = instance.render()
-    patch(subTree,container)
+    patch(subTree, container)
 }
