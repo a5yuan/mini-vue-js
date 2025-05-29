9d8fcb3da262b837326159a12991e4421f64eba2
a5yuan <134692219+a5yuan@users.noreply.github.com>
2025-05-29 15:54:08 +0800

    slot 案例

---
+++ example/slot/App.js
@@ -0,0 +1,22 @@
+
+import { h } from "../../lib/guide-mini-vue.es.js"
+import { foo } from "./foo.js"
+export const App = {
+    //* vue3
+    //*  template
+    //* rend
+    name: 'App',
+    render() {
+        const App = h('div',{},'App')
+        const Foo = h(foo)
+        return h(
+            'div',
+            {},
+            [App,Foo]
+        )
+    },
+    setup() {
+
+        return {}
+    }
+}
---
+++ example/slot/foo.js
@@ -0,0 +1,12 @@
+
+import { h } from "../../lib/guide-mini-vue.es.js"
+
+
+export const foo = {
+    name:'foo',
+    setup() {
+    },
+    render() {
+        return h('div', {},'foo')
+    }
+}
